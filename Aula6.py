a1cb6c098df6ba5a4c18be9a96e1f63663f5f1c2
Diego99xz <diego.melo99xz@gmail.com>
2024-12-02 19:48:49 -0300

    adicionado Aula 6 e Desafio 4

---
+++ Aula6.py
@@ -0,0 +1,15 @@
+print('====== Aula 006 ======')
+#Tipos Primitivos
+
+#int
+#str
+#float
+#str
+
+n = input('Digite um valor: ')
+print(type(n))
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isdecimal())
+print(n.islower())
+print(n.isupper())
