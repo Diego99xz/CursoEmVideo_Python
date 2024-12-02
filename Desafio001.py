1141b63467bccfb1632bc9edda88b9bddbe16378
Diego99xz <diego.melo99xz@gmail.com>
2024-12-02 17:20:01 -0300

    aula 1 a 5 e desafios 1 a 3

---
+++ Desafio001.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome? ')
+
+#print('Olá ',nome,'! Prazer em te conhecer!')
+print(f'Olá {nome}! Prazer em te conhecer!')
+#print('Olá {}! Prazer em te conhecer!'.format(nome))
---
+++ Desafio003.py
@@ -0,0 +1,6 @@
+print('====== DESAFIO 003 ======')
+
+n1 = int(input('Primeiro Número:'))
+n2 = int(input('Segundo Número:'))
+
+print(f'A soma é {n1 + n2}')
