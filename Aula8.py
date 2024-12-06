959846a618c9d49d9aa0865864f3b72319b6b44a
Diego99xz <diego.melo99xz@gmail.com>
2024-12-05 21:21:21 -0300

    Aula 8

---
+++ Aula8.py
@@ -0,0 +1,9 @@
+print('====== Aula 008 ======')
+
+import math # importando a biblioteca
+from math import sqrt #importando somente a funcao da biblioteca
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+
+print('A raiz de {} é igual a {}'.format(num, raiz))
