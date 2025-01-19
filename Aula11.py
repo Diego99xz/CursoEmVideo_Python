a26a49728cfeea9a43b3f1c4b5a4bb1fe045eb71
Diego99xz <diego.melo99xz@gmail.com>
2025-01-19 12:20:58 -0300

    Adicionado aula extra 11 e aplicado cores em exercicos

--- Aula11.py
+++ Aula11.py
@@ -2,3 +2,57 @@ print('====== Aula 011 ======')
 
 #Aula Extra
 #Cores no terminal
+
+#código para colocar cor
+#\033[Estilo da fonte;cor do texto;Cor de fundo m
+
+#Código estilo da fonte
+# 0 - None
+# 1 - Bold
+# 4 - Underline
+# 7 - Negative
+
+#Código cor de texto
+# 30 - Branco
+# 31 - Vermelho
+# 32 - Verde
+# 33 - Amarelo
+# 34 - Azul
+# 35 - Roxo
+# 36 - Ciano
+# 37 - Cinza
+
+#Código cor de fundo
+# 40 - Branco
+# 41 - Vermelho
+# 42 - Verde
+# 43 - Amarelo
+# 44 - Azul
+# 45 - Roxo
+# 46 - Ciano
+# 47 - Cinza
+
+'''
+Exemplos:
+\033[0;30;41:m
+\033[4;33;44m
+\033[1;35;43m
+\033[30;42m
+\033[m
+\033[7;30m
+'''
+
+#Exemplo
+print('\033[1;31;43mOlá, Mundo!\033[m')
+
+a = 3
+b = 5
+print('OS Valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))
+
+nome = 'Diego'
+cores = {'limpa':'\033[m',
+         'vermelho':'\033[31m',
+         'amarelo':'\033[33m',
+         'pretoebranco':'\033[7:30m'}
+
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format(cores['vermelho'],nome,cores['limpa']))
--- Desafio001.py
+++ Desafio001.py
@@ -5,4 +5,8 @@ nome = input('Qual é seu nome? ')
 
 #print('Olá',nome,'! Prazer em te conhecer!')
 #print(f'Olá {nome}! Prazer em te conhecer!')
-print('Olá {}! Prazer em te conhercer'.format(nome))
+
+#Lsita de cores, 0 = fecha o cod, 1 = red, 2 = green, 3 = blue
+rgb = ['\033[m','\033[31m','\033[32m','\033[34m']
+
+print('Olá {}{}{}! Prazer em te conhercer'.format(rgb[2], nome, rgb[0] ))
