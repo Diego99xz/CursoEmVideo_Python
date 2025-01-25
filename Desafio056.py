3df20b13f9fa1aa6dfa9db481c214b84cfac3e37
Diego99xz <diego.melo99xz@gmail.com>
2025-01-25 15:21:49 -0300

    adicionado o desafio056

--- Desafio056.py
+++ Desafio056.py
@@ -2,25 +2,28 @@ print('====== DESAFIO 056 ======')
 
 #Desenvolva um progra que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
 
-lista_nomes = []
-lista_idades = []
-lista_sexo = []
-for i in range(1, 4+1):
-    print(f'Informe os Dados da {i}º Pessoa')
-    nome = str(input(f'Nome: '))
-    idade = int(input(f'Idade: '))
-    print('Qual o sexo ?\n[1]-Masculino\n[2]-Feminino')
-    sexo = int(input('Digite o número referente ao sexo: '))
-    print('---------------------------------------------')
-    if sexo == 1 or sexo == 2:
-        lista_nomes.append(nome)
-        lista_idades.append(idade)
-        lista_sexo.append(sexo)
-    else:
-        print('Número de Sexo errado!')
-        print('Programa Encerrado!')
-        break
+soma_idades=0
+old_man = {"nome": "", "idade": 0}
+mulheres_menos_20 = 0
 
-media = (lista_idades[0] + lista_idades[1] + lista_idades[2]+ lista_idades[3]+ lista_idades[4]) / len(lista_idades)
-mais_velho = max(lista_idades)
-mais_velho = lista_nomes
+for i in range(1, 5+1):
+    print(f'Informações da {i}º Pessoa')
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('[M]-Masculino\n[F]-Feminino\nInforme a letra do seu sexo: '))
+    sexo = sexo.upper()
+
+    soma_idades = soma_idades + idade
+
+    if sexo ==  'M' and idade > old_man["idade"]:
+        old_man["nome"] = nome
+        old_man["idade"] = idade
+
+    if sexo == 'F' and idade < 20:
+        mulheres_menos_20 += 1
+
+media = soma_idades/5
+
+print(f'A Média de didade do grupo é {media}')
+print(f'O Homem mais velho é o {old_man["nome"]}')
+print(f'Quantidade de mulheres com menos de 20 anos:  {mulheres_menos_20}')
