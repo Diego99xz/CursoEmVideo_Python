d5e417d8381acc9ad0177c5dd6b82b76c6172450
Diego99xz <diego.melo99xz@gmail.com>
2025-03-31 20:26:44 -0300

    Desafio078

--- Desafio078.py
+++ Desafio078.py
@@ -2,3 +2,14 @@ print('====== DESAFIO 078 ======')
 
 # Faça um programa que leia 5 valores numérios e guarde-os em uma lista.
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições
+
+lista = []
+
+for i in range(0, 5):
+    n = int(input(f'Digite um valor para a posição {i}: '))
+    lista.append(n)
+
+print(20*'-=-=-')
+print(f'Você Digitou os valores {lista}')
+print(f'O maior valor digitado foi {max(lista)} nas posições {lista.index(max(lista))}...')
+print(f'O menor valor digitado foi {min(lista)} nas posições {lista.index(min(lista))}...')
