553b1973cbd86aa57588f9002ebe092daeb4ab94
Diego99xz <diego.melo99xz@gmail.com>
2025-04-01 22:30:24 -0300

    Desafio 79 e 80

--- Desafio079.py
+++ Desafio079.py
@@ -4,3 +4,20 @@ print('====== DESAFIO 079 ======')
 # caso o número já exista lá dentro, ele não será adicionado.
 # No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 
+continuar = 's'
+lista = []
+
+while continuar == 's':
+    valor = int(input('Digite um valor: '))
+    
+    if lista.count(valor):
+        print('Valor duplicado! Não vou adicionar...')
+    else:
+        print('Valor adicionado com sucesso...')
+        lista.append(valor)
+
+
+    continuar = str(input('Quer continuar? [S/N] ')).lower()
+
+print(20*'-=-=-')
+print(f'Você digitou os valores {sorted(lista)}')
