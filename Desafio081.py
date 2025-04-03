67496e296b3eb8a8c86069a987b48384da85c7df
Diego99xz <diego.melo99xz@gmail.com>
2025-04-03 08:36:35 -0300

    desafio 81

--- Desafio081.py
+++ Desafio081.py
@@ -5,3 +5,21 @@ print('====== DESAFIO 081 ======')
 # B)A lista de valores, ordenada de forma decrescente.
 # C)Se o valor 5 foi digitados e está ou não na lista.
 
+lista = []
+
+while True:
+    valor = int(input('Digite um valor: '))
+    lista.append(valor)
+
+    continuar = input('Quer continuar? [S/N] ').strip().lower()
+    if continuar != 's':
+        break
+
+lista.sort(reverse=True)  # Ordena a própria lista
+
+print(f'Você digitou {len(lista)} elementos.')
+print(f'Os valores em ordem decrescente são {lista}')  # Imprime a lista corretamente
+if 5 in lista:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não está na lista!')
