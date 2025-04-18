ddfadecb0bd15f7dccb048ff114c4c43aa17c1b4
Diego99xz <diego.melo99xz@gmail.com>
2025-04-18 17:32:57 -0300

    Desafio 83

--- Desafio082.py
+++ Desafio082.py
@@ -3,3 +3,25 @@ print('====== DESAFIO 082 ======')
 # Crie um programa que vai ler vários números e colocar em uma lista.
 # Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores impares digitados, respectivamente.
 # Ao final, mostre o contúdo das três listas geradas.
+
+lista = []
+lista_par = []
+lista_impar = []
+continuar = 'S'
+
+while continuar == 'S':
+    numero = int(input('Digite um número: '))
+    lista.append(numero)
+    
+    continuar = str(input('Quer Continuar? [S/N] ')).upper()
+
+for i in range(0, len(lista)):
+
+    if lista[i] % 2 == 0:
+        lista_par.append(lista[i])
+    else:
+        lista_impar.append(lista[i])
+
+print(f'Lista Completa:{lista}')
+print(f'Lista Pares: {lista_par}')
+print(f'Lista impares: {lista_impar}')
--- Desafio083.py
+++ Desafio083.py
@@ -3,3 +3,19 @@ print('====== DESAFIO 083 ======')
 # Crie um programa onde o usuário digite um expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
+lista = input('Digite a expressão: ')
+count_abre_parenteses = 0
+count_fecha_parenteses = 0
+
+for i in range(0, len(lista)):
+
+    if lista[i] == '(':
+        count_abre_parenteses += 1
+    
+    if lista[i] == ')':
+        count_fecha_parenteses += 1
+
+if count_abre_parenteses == count_fecha_parenteses:
+    print('Sua expressão está correta!')
+else:
+    print('Sua expressão está errada!')
