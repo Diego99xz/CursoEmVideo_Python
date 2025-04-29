86e2920c94980f2ae96d6ad3a2a77b7dfb6606e3
Diego99xz <diego.melo99xz@gmail.com>
2025-04-29 20:56:41 -0300

    Aula 21

--- Aula21.py
+++ Aula21.py
@@ -59,5 +59,38 @@ n = 2
 print(f'No programa principal, n vale {n}')
 teste()
 
+#--------------------------------------------------------------------
+# Retorno de valores
+def somar(a =0, b=0, c=0):
+    s= a+ b+ c
+    return s
 
-#30min video aula
+r1 = somar(3, 2, 5)
+r2 = somar(2, 2)
+r3 = somar()
+print(f'Os resultados foram {r1}, {r2} e {r3}')
+#--------------------------------------------------------------------
+
+def fatorial(num = 1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
+
+#--------------------------------------------------------------------
+def par(n=0):
+    if n % 2 ==0:
+        return True
+    else: 
+        return False
+    
+num  = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('Não é par!')
