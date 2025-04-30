dcb59a31c7a7e67199e258ccd8938fb4deaae125
Diego99xz <diego.melo99xz@gmail.com>
2025-04-30 18:52:37 -0300

    Adicionado Desafio101

---
+++ Desafio101.py
@@ -0,0 +1,21 @@
+print('====== DESAFIO 101 ======')
+
+# Crie um programa que tenha uma função chamada voto() que vai receber como
+# parâmetro o ano de nascimento de uma pessoa, retornando um valor literal
+# indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições
+
+import datetime
+
+def voto(ano):
+    data_atual = datetime.date.today().year
+    idade = data_atual - ano
+    if idade >= 18:
+        print(f'Com {idade} anos: VOTO OBRIGATÓRIO')
+    elif idade >= 16:
+        print(f'Com {idade} anos: VOTO OPCIONAL')
+    else:
+        print(f'Com {idade} anos: VOTO NEGADO')
+
+# PROGRAMA PRINCIPAL
+ano = int(input('Em que ano você nasceu? '))
+voto(ano)
---
+++ Desafio102.py
@@ -0,0 +1,6 @@
+print('====== DESAFIO 102 ======')
+
+# Crie um programa que tenha uma função fatorial() que receba dois parâmentros:
+# o primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico
+# (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
