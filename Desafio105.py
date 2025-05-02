fa49bafc7631ae3a8b6b574a14335b6a271aae0a
Diego99xz <diego.melo99xz@gmail.com>
2025-05-02 19:05:47 -0300

    desafio105

--- Desafio105.py
+++ Desafio105.py
@@ -9,3 +9,31 @@ print('====== DESAFIO 105 ======')
 # - A situação (Opcional)
 
 # Adicione também as docStrings da função.
+
+def notas(*n, sit=False):
+    """
+    ->Função para anlisar notas e situações de vários alunos.
+    :param n: uma ou mais notas do alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n)/len(n)
+
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+# Programa Principal
+resp = notas(5.5, 2.5, 8.5, sit=True)
+print(resp)
+help(notas)
