640c1cef59dc86065f4a077b863c07fe1ec4e103
Diego99xz <diego.melo99xz@gmail.com>
2025-05-04 18:30:14 -0300

    Desafio 113-115

--- Aula23.py
+++ Aula23.py
@@ -7,8 +7,22 @@ try:
     a = int(input('Numerador: '))
     b = int(input('Denominador: '))
     r = a / b
-except:
-# except é o que apresentará se falhar
-    print('Infelizmente tivemos um problema :( ')
+# except:
+# # except é o que apresentará se falhar
+#     print('Infelizmente tivemos um problema :( ')
+except (ValueError, TypeError) as erro:
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+    print(f'Problema encontrado foi {erro.__class__}')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
+except Exception as erro:
+    print(f'Problema encontrado foi {erro.__class__}')
+else:
+# Se der certo faça...    
+    print(f'O resultado é {r:.1f}')
 
-print(f'O resultado é {r}')
+finally:
+# Idependente se deu certo ou erro vai ocorrer
+    print('Volte sempre! Muito obrigado!')
